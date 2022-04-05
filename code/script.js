5e3413e599055128faf140141dbf732ad425f467
unknown <jjy20315@gmail.com>
2022-04-05 12:48:03 +0900

    async/await apply

--- code/script.js
+++ code/script.js
@@ -1,30 +1,26 @@
 const apikey = "RGAPI-a60c9f12-8e05-4c43-afc8-7852fef7e305";
 const nickname = "쌕주랜드";
 const GetID_url = `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${nickname}?api_key=${apikey}`;
-const ID = { id: "" };
+const ID = Whoareyou();
 const MC = {};
 //SUMMONER By userNickName
 function Whoareyou() {
-  console.log("whoareyou시작");
-  fetch(GetID_url)
-    .then((res) => res.json())
-    .then((data) => {
-      ID.id = data.id;
-      console.log("whoareyou fetch than", data.id);
-    });
+  const response = fetch(GetID_url);
+  return response.then((res) => res.json());
 }
 
-function MChampion() {
-  const GETMC_url = `https://kr.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${ID.id}?api_key=${apikey}`;
-  console.log(GETMC_url);
-  fetch(GETMC_url)
-    .then((res) => res.json())
-    .then((data) => (MC = data));
+//챔피언 목록 요청
+async function MChampion() {
+  try {
+    const userID = await Whoareyou();
+    const GETMC_url = `https://kr.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${userID.id}?api_key=${apikey}`;
+    const champion = fetch(GETMC_url).then((res) => res.json());
+    console.log(champion);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 console.log("전역 시작");
-Whoareyou();
-console.log(ID);
 MChampion();
-console.log(MC);
 console.log("전역 끝");
