c33cdd1768f4aa688e033ca991cf9b285c861f01
JKMA-3 <jjy20315@gmail.com>
2022-04-07 01:49:06 +0900

    css Update

--- code/render/render.js
+++ code/render/render.js
@@ -1,14 +1,16 @@
 const render_result = (userID, champion) => {};
 const search_form = () => {
+  //const img_path = require("../../img/Logo/Logo.PNG");
+
   let search_temp = `<div class="flex justify-center h-screen justify-center bg-blue-400">
     <div class="box-content h-1/2 w-1/3 fixed bg-white absolute inset-y-1/4 rounded-lg shadow-lg">
         <div>
             <div class="box-content h-2/3 w-full justify-center">
-            <h1>asd</h1>
+              <img class="object-contain h-min w-min" src="../../img/Logo/Logo.jpg">
             </div>
-            <div>
-            <input type="text" class=" rounded border-2 border-blue-500">
-            <button class="border-4 "></button>
+            <div class="">
+              <input type="text" class="rounded border-2 border-black">
+              <button class="border-2 bg-gray-300  ">검색</button>
             </div>
         </div>
     </div>
