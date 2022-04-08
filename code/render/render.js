81de1febb4e0a74fd365c16180de7c0274803202
unknown <jjy20315@gmail.com>
2022-04-08 13:30:32 +0900

    search-form css update

--- code/render/render.js
+++ code/render/render.js
@@ -1,18 +1,22 @@
-const render_result = (userID, champion) => {};
+const render_result = (userID, champion) => {
+  let result_form = `
+  <div><p>${userID}</p></div>
+  `;
+
+  root.innerHTML = result_form;
+};
 const search_form = () => {
   //const img_path = require("../../img/Logo/Logo.PNG");
 
-  let search_temp = `<div class="flex justify-center h-screen justify-center bg-blue-400">
-    <div class="box-content h-1/2 w-1/3 fixed bg-white absolute inset-y-1/4 rounded-lg shadow-lg">
-        <div>
-            <div class="box-content h-2/3 w-full justify-center">
-              <img class="object-contain h-min w-min" src="../../img/Logo/Logo.jpg">
-            </div>
-            <div class="">
-              <input type="text" class="rounded border-2 border-black">
-              <button class="border-2 bg-gray-300  ">검색</button>
-            </div>
-        </div>
+  let search_temp = `<div class="flex justify-center h-screen bg-blue-400">
+    <div class=" box-content h-1/2 w-1/4 fixed bg-white absolute inset-y-1/4 rounded-lg shadow-xlg">
+      <div class=" box-content h-2/3 w-full grid justify-items-center">
+        <img class=" justify-self-center" src="../../img/Logo/Logo.jpg" style="width:200px; height:200px;">
+      </div>
+      <div class="">
+        <input type="text" class="rounded border-2 border-black">
+        <button class="border-2 bg-blue-400 rounded-lg"><font> 검 색 </font></button>
+      </div>
     </div>
   </div>`;
   root.innerHTML = search_temp;
