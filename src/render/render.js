11a0e731f4e8ab31d66718347a6a1f5e928470fa
unknown <jjy20315@gmail.com>
2022-04-13 17:48:21 +0900

    render.js css update

--- src/render/render.js
+++ src/render/render.js
@@ -39,12 +39,12 @@ const renderResult = (resultValue) => {
         </div>
       </div>
     </div>
-  `;
-
+    `;
   root.innerHTML = resultForm;
 };
 
 //자바스크립트 컨벤션
+
 //let키워드 사용을 지양해야 하는 이유
 // 변수 초기화 시점부터 값 할당 이전 까지를 일시적 사각지대(TDZ)라 하며 이의 존재 여부가 var 키워드와 let/const키워드의 차이점이며 var키워드를 사용하면 안되는 이유이기도 하다. 또한 let/const의 차이점은  값의 재할당 가능 여부이다. 재할당이 필요한 변수인 경우에만 한하여 let키워드를 사용하며 이를 제외한 경우에는 const 키워드를 사용하여 의도치 않은 재할당을 방지하여야 한다.
 const searchForm = () => {
